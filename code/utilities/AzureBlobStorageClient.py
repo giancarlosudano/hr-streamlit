fd1ffea50fe98809ff03303b0dae137c282fa2f0
Giancarlo Sudano <gisudano@microsoft.com>
2023-05-07 09:10:24 +0200

    begin to calulate mathcing

--- code/utilities/AzureBlobStorageClient.py
+++ code/utilities/AzureBlobStorageClient.py
@@ -4,57 +4,72 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, g
 from dotenv import load_dotenv
 
 class AzureBlobStorageClient:
-    def __init__(self, account_name: str = None, account_key: str = None, container_name: str = None):
+    def __init__(self, account_name: str = None, account_key: str = None):
 
         load_dotenv()
 
         self.account_name : str = account_name if account_name else os.getenv('BLOB_ACCOUNT_NAME')
         self.account_key : str = account_key if account_key else os.getenv('BLOB_ACCOUNT_KEY')
         self.connect_str : str = f"DefaultEndpointsProtocol=https;AccountName={self.account_name};AccountKey={self.account_key};EndpointSuffix=core.windows.net"
-        self.container_name : str = container_name if container_name else os.getenv('BLOB_CONTAINER_NAME')
         self.blob_service_client : BlobServiceClient = BlobServiceClient.from_connection_string(self.connect_str)
 
-    def upload_file(self, bytes_data, file_name, content_type='text/plain'):
-        # Create a blob client using the local file name as the name for the blob
-        blob_client = self.blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
-        # Upload the created file
+    def upload_file(self, bytes_data, file_name: str, container_name: str, content_type: str):
+        blob_client = self.blob_service_client.get_blob_client(container=container_name, blob=file_name)
+        
         blob_client.upload_blob(bytes_data, overwrite=True, content_settings=ContentSettings(content_type=content_type))
-        # Generate a SAS URL to the blob and return it
-        return blob_client.url + '?' + generate_blob_sas(self.account_name, self.container_name, file_name,account_key=self.account_key,  permission="r", expiry=datetime.utcnow() + timedelta(hours=3))
+        return blob_client.url + '?' + generate_blob_sas(self.account_name, container_name, file_name,account_key=self.account_key,  permission="r", expiry=datetime.utcnow() + timedelta(hours=3))
     
-    def upload_cv(self, bytes_data, file_name, content_type='text/plain'):
-        # Create a blob client using the local file name as the name for the blob
-        blob_client = self.blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
-        # Upload the created file
-        blob_client.upload_blob(bytes_data, overwrite=True, content_settings=ContentSettings(content_type=content_type))
-        # Generate a SAS URL to the blob and return it
-        return blob_client.url + '?' + generate_blob_sas(self.account_name, self.container_name, file_name,account_key=self.account_key,  permission="r", expiry=datetime.utcnow() + timedelta(hours=3))
-
     def download_blob_to_string(self, blob_service_client: BlobServiceClient, container_name, blob_name):
         blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
         downloader = blob_client.download_blob(max_concurrency=1, encoding='UTF-8')
         blob_text = downloader.readall()
-        return blob_text 
+        return blob_text
 
-    def get_all_files(self, container_name):
+    def get_all_files(self, container_name: str):
 
         container_client = self.blob_service_client.get_container_client(container_name)
         blob_list = container_client.list_blobs(include='metadata')
         
-        sas = generate_container_sas(self.account_name, self.container_name,account_key=self.account_key,  permission="r", expiry=datetime.utcnow() + timedelta(hours=3))
+        sas = generate_container_sas(self.account_name, 
+                                     container_name, 
+                                     account_key=self.account_key,  
+                                     permission="r", 
+                                     expiry=datetime.utcnow() + timedelta(hours=3))
         
         files = []
         for blob in blob_list:
             
-            downloader = container_client.download_blob(blob=blob.name, max_concurrency=1, encoding='UTF-8')
+            downloader = container_client.download_blob(blob=blob.name, 
+                                                        max_concurrency=1, 
+                                                        encoding='UTF-8')
             
             files.append({
-                "File" : blob.name,
-                "Content" : downloader.readall(),
-                "Matching" : 0, 
+                "file" : blob.name,
+                "content" : downloader.readall(),
+                "matching" : 0, 
                 "fullpath": f"https://{self.account_name}.blob.core.windows.net/{self.container_name}/{blob.name}?{sas}",                
                 })
         return files
+
+    def get_all_urls(self, container_name: str):
+
+        container_client = self.blob_service_client.get_container_client(container_name)
+        blob_list = container_client.list_blobs(include='metadata')
+        
+        sas = generate_container_sas(self.account_name, 
+                                     container_name, 
+                                     account_key=self.account_key,  
+                                     permission="r", 
+                                     expiry=datetime.utcnow() + timedelta(hours=3))
+        
+        urls = []
+        for blob in blob_list:    
+            urls.append({
+                "file" : blob.name, 
+                "fullpath": f"https://{self.account_name}.blob.core.windows.net/{container_name}/{blob.name}?{sas}",                
+                })
+        return urls
+    
     
     def upsert_blob_metadata(self, file_name, metadata):
         blob_client = BlobServiceClient.from_connection_string(self.connect_str).get_blob_client(container=self.container_name, blob=file_name)
